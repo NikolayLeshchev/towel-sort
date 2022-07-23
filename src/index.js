5757e3eb4f045b87cbf536d43074b7216c22f17f
Nikolai <82165109+NikolayLeshchev@users.noreply.github.com>
2022-07-23 17:50:14 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 
 function towelSort(arr) {
-  let res = [];
-  res = `[${arr.join(",")}]`;
-
+  let res = [arr[0]];
+  for (let i = 1; i < arr.length; i++) {
+    res = `${res},${arr[i]}`;
+  }
   return res;
 }
 
