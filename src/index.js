1f0711bb4bddc77da3e9c4cd1b3e46a77add6c91
Nikolai <82165109+NikolayLeshchev@users.noreply.github.com>
2022-07-23 18:18:57 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 function towelSort(arr) {
+  
   let res = [];
-  for (let i = 0; i < arr.length; i++) {
-    i % 2 === 0 ? (res = res.concat(matrix[i])) : (res = res.concat(matrix[i].reverse()));
+
+  if (matrix.length <= 0) {
+    return res;
+  } else {
+    for (let i = 0; i < arr.length; i++) {
+      i % 2 === 0
+        ? (res = res.concat(matrix[i]))
+        : (res = res.concat(matrix[i].reverse()));
+    }
   }
   return res;
 }
