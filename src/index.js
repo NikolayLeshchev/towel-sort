581228844a854d0b26f1919de16e20c86c13497b
Nikolai <82165109+NikolayLeshchev@users.noreply.github.com>
2022-07-23 17:58:26 +0300

    feat: ready function

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-
 function towelSort(arr) {
   let res = [arr[0]];
   for (let i = 1; i < arr.length; i++) {
-    res = `${res},${arr[i]}`;
+    if (i % 2 === 0) {
+      res = `${res},${arr[i]}`;
+    } else {
+      res = `${res},${arr[i].reverse()}`;
+    }
   }
   return res;
 }
